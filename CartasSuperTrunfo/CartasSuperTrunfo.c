6dbcf83f2308f402e8c36833bfb187a2e71fb575
Albru31 <alex.phouse@outlook.pt>
2025-04-24 01:48:37 +0000

    Nivel Aventureiro

--- CartasSuperTrunfo/CartasSuperTrunfo.c
+++ CartasSuperTrunfo/CartasSuperTrunfo.c
@@ -7,15 +7,17 @@
 int main() {
     /*Parte inicial do programa, onde sao declaradas todas as variaveis 
     (tipo nome_da_variavel[N]) N=Numero de caracteres quando string*/
-    char estado01[50] = "A";
-    char estado02[50] = "B"; 
+    char estado01[60] = "A";
+    char estado02[60] = "B"; 
     char codigo01[5] = "A01";
     char codigo02[5] = "B01";
-    char cidade01[50] = "a";
-    char cidade02[50] = "b";
+    char cidade01[60] = "a";
+    char cidade02[60] = "b";
     int populacao01 = 0, populacao02 = 0;
     float area01 = 0, area02 = 0;
-    float pib01 = 0, pib02 = 0;       
+    float densidade01 = 0, densidade02 = 0;
+    float pib01 = 0, pib02 = 0;   
+    float renda01 = 0, renda02 = 0;    
     int pTuristicos01 = 0, pTuristicos02 = 0;
     
     /* printf - escreve no terminal
@@ -23,10 +25,13 @@ int main() {
     \n - pula linha no terminal
     %s - string
     %d - inteiro
-    %f - flutuante - que possui casas decimais    */
-    printf("Insira as informaçoes da primeira carta - \n"); //Solicita no terminal as informaçoes da carta 01
+    %f - flutuante - que possui casas decimais    
+    = - iguala as variaveis
+    / - divide as variaveis  */
+
+    printf("Insira as informaçoes da primeira carta - \n"); //Solicita no terminal as informaçoes da carta 01.
     printf("Estado: ");
-    scanf("%s", &estado01);
+    scanf("%s", &estado01); 
     printf("Código: ");
     scanf("%s", &codigo01);
     printf("Cidade: ");
@@ -35,13 +40,16 @@ int main() {
     scanf("%d", &populacao01);
     printf("Área: ");
     scanf("%f", &area01);
+    densidade01 = populacao01 / area01;  // Dividindo a populaçao pela area se obtem a densidade populacional.
     printf("PIB: ");
     scanf("%f", &pib01);
+    renda01 = populacao01 / area01;  // Dividindo o PIB pela populaçao se obtem a renda percapta.
     printf("Número de pontos turisticos: ");
     scanf("%d", &pTuristicos01);
 
+
     printf("\n");
-    printf("Insira as informaçoes da segunda carta - \n"); //Solicita no terminal as informaçoes da carta 2
+    printf("Insira as informaçoes da segunda carta - \n"); //Solicita no terminal as informaçoes da carta 2.
     printf("Estado: ");
     scanf("%s", &estado02);
     printf("Código: ");
@@ -52,26 +60,30 @@ int main() {
     scanf("%d", &populacao02);
     printf("Área: ");
     scanf("%f", &area02);
+    densidade02 = populacao02 / area02;  // Dividindo a populaçao pela area se obtem a densidade populacional.
     printf("PIB: ");
     scanf("%f", &pib02);
+    renda02 = populacao02 / pib02;  // Dividindo o PIB pela populaçao se obtem a renda percapta.
     printf("Número de pontos turisticos: ");
     scanf("%d", &pTuristicos02);
 
     /*Escreve no terminal todas as informçoes salvas nas variaveis referente a carta 01*/
     printf("\n");
-    printf("_____________________________\n");
-    printf("INFORMAÇOES\n");
+    printf("**** Suas Cartas sao ****\n");
+    printf("\n");
     printf("Carta 01\n");
     printf("Estado: %s\n", estado01);
     printf("Código: %s\n", codigo01);
     printf("Cidade: %s\n", cidade01);
     printf("População: %d habitantes\n", populacao01);
     printf("Área: %.2f km²\n", area01); // %.2f - O ".2" define o numero de casas decimais de escrita
-    printf("PIB: R$ %.2f\n", pib01);
-    printf("Número de pontos turísticos: %d\n", pTuristicos01);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade01);
+    printf("PIB: %.2f bilhoes de reais\n", pib01);
+    printf("Renda Percapta: R$ %.2f\n", renda01);
+    printf("Número de Pontos Turísticos: %d\n", pTuristicos01);
     printf("\n");
 
-    /*Escrve no terminal todas as informaçoes salvas nas variaveis referente a carta 02*/
+    /*Escreve no terminal todas as informaçoes salvas nas variaveis referente a carta 02*/
     printf("\n");
     printf("Carta 02 \n");
     printf("Estado: %s \n", estado02);
@@ -79,7 +91,9 @@ int main() {
     printf("Cidade: %s \n", cidade02);
     printf("População: %d habitantes\n", populacao02);
     printf("Área: %.2f km² \n", area02); // %.2f - O ".2" define o numero de casas decimais de escrita
-    printf("PIB: R$ %.2f \n", pib02);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade02);
+    printf("PIB: %.2f bilhoes de reais\n", pib02);
+    printf("Renda Percapta: R$ %.2f\n", renda02);
     printf("Número de pontos turísticos: %d \n", pTuristicos02);
 
     return 0;
