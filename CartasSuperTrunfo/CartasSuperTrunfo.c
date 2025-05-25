514fa56b6123ba344e62b9d77b31e3faf577ff91
Albru31 <alex.phouse@outlook.pt>
2025-05-25 15:35:44 +0000

    Cumprido desafio nivel Mestre

--- CartasSuperTrunfo/CartasSuperTrunfo.c
+++ CartasSuperTrunfo/CartasSuperTrunfo.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
 // Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 
 int main() {
-    /*Parte inicial do programa, onde sao declaradas todas as variaveis 
-    (tipo nome_da_variavel[N]) N=Numero de caracteres quando string*/
+    // Parte inicial do programa, onde sao declaradas todas as variaveis
+    // (tipo nome_da_variavel[N]) N=Numero de caracteres quando string
     char estado01[30];
     char estado02[30]; 
     char codigo01[4];
@@ -21,18 +19,14 @@ int main() {
     int pTuristicos01 = 0, pTuristicos02 = 0;
     float poder01;
     float poder02;
-        
-    /* printf - escreve no terminal
-    scanf - salva o que foi escrito no terminal na variavel
-    \n - pula linha no terminal
-    %s - string
-    %d - inteiro
-    %f - flutuante - que possui casas decimais    
-    = - iguala as variaveis
-    / - divide as variaveis  */
+    int escolha01, escolha02;
+    int final;
+    int resultado01, resultado02;
 
-    printf("Insira as informaçoes da primeira carta - \n"); //Solicita no terminal as informaçoes da carta 01.
-    printf("Estado: ");
+    //Solicita no terminal as informaçoes da carta 01.
+    printf("**** Cadastro de Cartas ****\n");
+    printf("Insira as informaçoes da primeira carta - \n"); 
+    printf("Estado : ");
     scanf("%s", &estado01); 
     printf("Código: ");
     scanf("%s", &codigo01);
@@ -49,9 +43,9 @@ int main() {
     printf("Número de pontos turisticos: ");
     scanf("%d", &pTuristicos01);
 
-
+    //Solicita no terminal as informaçoes da carta 2.
     printf("\n");
-    printf("Insira as informaçoes da segunda carta - \n"); //Solicita no terminal as informaçoes da carta 2.
+    printf("Insira as informaçoes da segunda carta - \n"); 
     printf("Estado: ");
     scanf("%s", &estado02);
     printf("Código: ");
@@ -87,7 +81,7 @@ int main() {
     printf("Poder: %.2f \n", poder01);
     printf("\n");
 
-    /*Escreve no terminal todas as informaçoes salvas nas variaveis referente a carta 02*/
+    //Escreve no terminal todas as informaçoes salvas nas variaveis referente a carta 02
     printf("\n");
     printf("Carta 02 \n");
     printf("Estado: %s \n", estado02);
@@ -103,29 +97,192 @@ int main() {
     printf("Poder: %.2f \n", poder02);
     printf("\n");
 
-    // Comparaçao de Cartas
-    printf("*** Comparaçao de Cartas ***\n");
-    printf("%s (%s): %d\n", cidade01, estado01, populacao01);
-    printf("%s (%s): %d\n", cidade02, estado02,  populacao02);
-    printf(" \n");
 
-    // Executa uma condiçao sobre uma comparaçao. 
-    // Se o valor de "populacao01" for maior que o valor de "populaçao02", entao...
-    // > - sinal de maior que
-    if (populacao01 > populacao02)
+    // Abre para o jogador escolher o primeiro atributo para a batalha
+    printf("### Batalha de Cartas ###\n");
+    printf("Escolha o primeiro atributo para a batalha\n");
+    printf("1- População, 2- Área, 3- PIB, 4- Renda Percapta, 5- Densidade, 6- Pontos Turísticos): \n");
+    scanf("%d", &escolha01);
+    printf("###  Primeira batalha  ###\n");
+
+    // Comparaçao de cartas
+    // A variavel resultado01 armazena o resultado da comparaçao entre os atributos escolhidos
+    switch (escolha01)
     {
-        printf("Carta 01 (%s) venceu!", cidade01);
+    case 1:     
+        printf("Carta 01 - %s\n", estado01);
+        printf("%d habitantes\n", populacao01);
+        printf("Carta 02 - %s\n", estado02);
+        printf("%d habitantes\n", populacao02);
+        if (populacao01 == populacao02) {
+            resultado01 = 0;
+        } else { resultado01 = populacao01 > populacao02 ? 1 : -1; }
+        break;
+    case 2:
+        printf("Carta 01 - %s\n", estado01);
+        printf("%.2f km²\n", area01);
+        printf("Carta 02 - %s\n", estado02);
+        printf("%.2f km²\n", area02);
+        if (area01 == area02) {
+            resultado01 = 0;
+        } else { resultado01 = area01 > area02 ? 1 : -1; }
+        break;
+    case 3:
+        printf("Carta 01 - %s\n", estado01);
+        printf("%.2f bilhoes de reais\n", pib01);
+        printf("Carta 02 - %s\n", estado02);
+        printf("%.2f bilhoes de reais\n", pib02);
+        if (pib01 == pib02) {
+            resultado01 = 0;
+        } else { resultado01 = pib01 > pib02 ? 1 : -1; }
+        break;
+    case 4:
+        printf("Carta 01 - %s\n", estado01);
+        printf("R$ %.2lf\n", renda01);
+        printf("Carta 02 - %s\n", estado02);
+        printf("R$ %.2lf\n", renda02);
+        if (renda01 == renda02) {
+            resultado01 = 0;
+        } else { resultado01 = renda01 > renda02 ? 1 : -1; }
+        break;
+    case 5:
+        printf("Carta 01 - %s\n", estado01);
+        printf("%.2f hab/km²\n", densidade01);
+        printf("Carta 02 - %s\n", estado02);
+        printf("%.2f hab/km²\n", densidade02);
+        if (densidade01 == densidade02) {
+            resultado01 = 0;
+        } else { resultado01 = densidade01 < densidade02 ? 1 : -1; }
+        break;
+    case 6:
+        printf("Carta 01 - %s\n", estado01);
+        printf("%d pontos turisticos\n", pTuristicos01);
+        printf("Carta 02 - %s\n", estado02);
+        printf("%d pontos turisticos\n", pTuristicos02);
+        if (pTuristicos01 == pTuristicos02) {
+            resultado01 = 0;
+        } else { resultado01 = pTuristicos01 > pTuristicos02 ? 1 : -1; }
+        break;
+    default:
+        break;
     }
-    // < - sinal de menor que
-    if (populacao01 < populacao02)
+
+    // Abre para o jogador escolher o segundo atributo para a batalha
+    printf("Escolha o segundo atributo para a batalha\n");
+    switch (escolha01)
     {
-        printf("Carta 02 (%s) venceu!", cidade02);
+    case 1:
+        printf("2- Área, 3- PIB, 4- Renda Percapta, 5- Densidade, 6- Pontos Turísticos): \n");
+        break;
+    case 2:
+        printf("1- População, 3- PIB, 4- Renda Percapta, 5- Densidade, 6- Pontos Turísticos): \n");
+        break;
+    case 3:
+        printf("1- População, 2- Área, 4- Renda Percapta, 5- Densidade, 6- Pontos Turísticos): \n");
+        break;
+    case 4:
+        printf("1- População, 2- Área, 3- PIB, 5- Densidade, 6- Pontos Turísticos): \n");
+        break;
+    case 5:
+        printf("1- População, 2- Área, 3- PIB, 4- Renda Percapta, 6- Pontos Turísticos): \n");
+        break;
+    case 6:
+        printf("1- População, 2- Área, 3- PIB, 4- Renda Percapta, 5- Densidade): \n");
+        break; 
+    default:
+        break;
     }
-    // == sinal de igualdade
-    if (populacao01 == populacao02)
+    scanf("%d", &escolha02);
+    
+    // Verifica se o jogador escolheu o mesmo atributo
+    if (escolha01 == escolha02) {
+        while (escolha01 == escolha02) {
+            printf("Escolha um atributo diferente do primeiro\n");
+            scanf("%d", &escolha02);
+        }   
+    }
+
+    // Comparaçao de cartas
+    printf("###  Segunda batalha  ###\n");
+    switch (escolha02)
     {
-        printf("Empate!");
+        case 1:     
+            printf("Carta 01 - %s\n", estado01);
+            printf("%d habitantes\n", populacao01);
+            printf("Carta 02 - %s\n", estado02);
+            printf("%d habitantes\n", populacao02);
+            if (populacao01 == populacao02) {
+                resultado02 = 0;
+            } else { resultado02 = populacao01 > populacao02 ? 1 : -1; }
+            break;
+        case 2:
+            printf("Carta 01 - %s\n", estado01);
+            printf("%.2f km²\n", area01);
+            printf("Carta 02 - %s\n", estado02);
+            printf("%.2f km²\n", area02);
+            if (area01 == area02) {
+                resultado02 = 0;
+            } else { resultado02 = area01 > area02 ? 1 : -1; }
+            break;
+        case 3:
+            printf("Carta 01 - %s\n", estado01);
+            printf("%.2f bilhoes de reais\n", pib01);
+            printf("Carta 02 - %s\n", estado02);
+            printf("%.2f bilhoes de reais\n", pib02);
+            if (pib01 == pib02) {
+                resultado02 = 0;
+            } else { resultado02 = pib01 > pib02 ? 1 : -1; }
+            break;
+        case 4:
+            printf("Carta 01 - %s\n", estado01);
+            printf("R$ %.2lf\n", renda01);
+            printf("Carta 02 - %s\n", estado02);
+            printf("R$ %.2lf\n", renda02);
+            if (renda01 == renda02) {
+                resultado02 = 0;
+            } else { resultado02 = renda01 > renda02 ? 1 : -1; }
+            break;
+        case 5:
+            printf("Carta 01 - %s\n", estado01);
+            printf("%.2f hab/km²\n", densidade01);
+            printf("Carta 02 - %s\n", estado02);
+            printf("%.2f hab/km²\n", densidade02);
+            if (densidade01 = densidade02) {
+                resultado02 = 0;
+            } else { resultado02 = densidade01 < densidade02 ? 1 : -1; }
+            break;
+        case 6:
+            printf("Carta 01 - %s\n", estado01);
+            printf("%d pontos turisticos\n", pTuristicos01);
+            printf("Carta 02 - %s\n", estado02);
+            printf("%d pontos turisticos\n", pTuristicos02);
+            if (pTuristicos01 == pTuristicos02) {
+                resultado02 = 0;
+            } else { resultado02 = pTuristicos01 > pTuristicos02 ? 1 : -1; }
+            break;
+        default:
+            break;
+
+    }
+    // Verifica o resultado da primeira batalha
+    if (resultado01 == 0) {
+        printf("Empate na primeira batalha\n");
+    } else { resultado01 > 0 ? printf("Carta 01 Venceu a primeira batalha\n") : printf("Carta 02 Venceu a primeira batalha\n");
+    }
+
+    // Verifica o resultado da segunda batalha
+    if (resultado02 == 0) {
+        printf("Empate na segunda batalha\n");
+    } else { resultado02 > 0 ? printf("Carta 01 Venceu a segunda batalha\n") : printf("Carta 02 Venceu a segunda batalha\n");
+    }
+
+    // Verifica o resultado final
+    final = resultado01 + resultado02;
+    if (final == 0) {
+        printf("Empate no jogo\n");
+    } else { final > 0 ? printf("Carta 01 Venceu o jogo\n") : printf("Carta 02 Venceu o jogo\n");
     }
 
     return 0;
 }
+
